444a4ecfc3d66937404c64d1c5ed0754c90aa06b
rekvizitt <chekaviyvlad@gmail.com>
2024-04-30 11:45:03 +0300

    Small fix voice_app.py

--- voice_api.py
+++ voice_api.py
@@ -8,10 +8,10 @@ user_input = args[1]
 
 client = Client("http://127.0.0.1:7860/")
 result = client.predict(
-		f"{user_input}",	# str  in 'Speechify this Text' Textbox component
-		"girl",	# Literal[Rogger]  in 'Select Speaker' Dropdown component
-		1.99,	# float (numeric value between 0.1 and 1.99) in 'Speed' Slider component
-		"Russian",	# Literal[Arabic, Chinese, Czech, Dutch, English, French, German, Hungarian, Italian, Japanese, Korean, Polish, Portuguese, Russian, Spanish, Turkish]  in 'Language/Accent' Dropdown component
+		f"{user_input}",
+		"girl",
+		1.99,
+		"Russian",
         api_name="/gen_voice"
 )
 print(result)
